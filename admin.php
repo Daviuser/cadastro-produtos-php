cf1d7dbcd64293a05e576214bf39ccb66fa3844e
Davi Santiago <davisantiago222@gmail.com>
2023-06-12 20:33:43 -0300

    foi mudado o titulo da pagina

--- admin.php
+++ admin.php
@@ -68,7 +68,7 @@ if(isset($_POST['update_produto'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Painel do Admin</title>
+   <title>Painel de Cadastro</title>
 
    <!-- link das fontes  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
